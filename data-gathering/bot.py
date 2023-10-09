eda22f73a55980d7ff4eee7580177bfdcdf8732b
Adetayo Boluwatife Chritiana <Samuelchristiana38@gmail.com>
2023-10-09 08:43:56 +0100

    Parametrize code variables in bot.py to match each Wikipedia's configuration file

--- data-gathering/bot.py
+++ data-gathering/bot.py
@@ -2,105 +2,88 @@
 '''
 Python script for performing various analyses on Wikipedia articles. 
 This script appears to collect data from Wikipedia and Wikidata, 
-process it, and save the results to a file. 
+process it, and save the results to a file named "resuls.txt". 
 '''
 
 import urllib
-
 import sys
-
 import calendar
-
 import json
-
 import datetime
-
 from urllib.request import urlopen
-
 import urllib.parse
-
 import string
-
-import json
-
 from datetime import datetime
 
 
-# subdomain of language, to be used for API calls
-language = "es"
+# Open and read the JSON configuration file and access the configuration data as a dictionary
+with open("wikipedia_config.json", "r") as config_file:
+   wikipedia_config = json.load(config_file)
 
-id_wikidata= 1
-
-dimension = 1
-
-first_edit = 1
-
-note  =  1
-
-images  = 1
-
-views = 1
-
-incipit_size = 1
-
-discussion_size = 1
-
-# discussion prefix
-discussion = "Discusión:"
-discussionURL = urllib.parse.quote(discussion)
+# Specify the Wikipedia language
+WIKIPEDIA_LANGUAGE = "it"  # Change this to the desired Wikipedia language, this for English
 
 
-warnings_config = 0
 
-common_pages = 1
+def main():
+   
+   if WIKIPEDIA_LANGUAGE in wikipedia_config:
+      language_config = wikipedia_config[WIKIPEDIA_LANGUAGE]
+   else:
+      print(f"Configuration not found for language '{WIKIPEDIA_LANGUAGE}'.") # Handle this case appropriately (e.g, exit the script).
+      
+   # Access configuration variables based on the language
+   file_to_be_analysed = language_config.get("file_to_be_analysed")
+   language = language_config.get("language")
+   id_wikidata = language_config.get("id_wikidata")
+   dimension = language_config.get("dimension")
+   first_edit = language_config.get("first_edit") 
+   note = language_config.get("note")
+   image = language_config.get("images")
+   views = language_config.get("views")
+   incipit_size = language_config.get("incipit_size")
+   discussion_size = language_config.get("discussion_size")
+   discussionURL = language_config.get("discussionURL")
+   warnings_config = language_config.get("warnings_config")
+   common_pages = language_config.get("common_pages")
+   common_gallery = language_config.get("common_gallery")
+   itwikisource = language_config.get("itwikisource")
+   wikiversity = language_config.get("wikiversity")
+   wikibooks = language_config.get("wikibooks")
+   featured_in = language_config.get("featured")
+   quality = language_config.get("quality")
+   review = language_config.get("review")
+   bibliography = language_config.get("bibliography")
+   coordinate = language_config.get("coordinate")
+   featured_template = language_config.get("featured_template")
+   display_window_template = language_config.get("display_window_template")
 
-common_gallery = 1
 
-itwikisource = 1
+   analysis(language, file_to_be_analysed, display_window_template, discussionURL,warnings_config, discussion_size, 
+      incipit_size, common_gallery,common_pages, itwikisource, coordinate, featured_template)
 
-wikiversity = 1
 
-wikibooks = 1
 
-featured = 1
 
-featured_template="{{artículo destacado" # featured template
-
-quality = 1
-
-template_quality="{{artículo bueno" # quality of template
-
-review= 1
-
-bibliography = 1
-
-coordinate = 1
 
 # Function to get average page views: returns visits since the beginning of time, average dayly visits since the begininning of time, average daily visits in the specified year
-def get_avg_pageviews(article_title, start, end):
+def get_avg_pageviews(article_title, start, end, language):
    SUM = 0
 
    try:
 
       url = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/"+language+".wikipedia/all-access/user/"+article_title+"/daily/"+start+"/"+end
-      
       html = urlopen(url).read()
-
       html = str(html)
-
       html = html.replace('{"items":[',"")
-
       html = html.replace(']}',"")
-
       n = html.count("}")
 
 
       for i in range(n):
 
          txt = html[:html.find("}")+1]
-
          SUM += int(txt[txt.find('"views":')+len('"views":'):-1])
-
          html =html.replace(txt,"",1)
       
       d1 = datetime.strptime(start, "%Y%m%d")
@@ -109,20 +92,17 @@ def get_avg_pageviews(article_title, start, end):
       result = str(int(round((SUM/days),0)))
 
    except:
-
       result = "ERROR"
    
    return result
 
-# returns visits since the beginning of time, average dayly visits since the begininning of time, average daily visits in the specified year
-def visit(article):
+# This function returns visits since the beginning of time, average dayly visits since the begininning of time, average daily visits in the specified year
+def visit(article, language):
 
-   #YYYYMMGG
+   #YYYYMMGG Date Format
    START_ALL_TIME = "20150701"; 
-
    START_PREV_YEAR = "20220101"; 
    END_PREV_YEAR = "20221231"; 
-
    START_CURRENT_YEAR = "20230101"; 
    END_CURRENT_YEAR   = "20230831"; 
 
@@ -131,11 +111,8 @@ def visit(article):
 
    #calculate result1, total pageviews since the beginning of time, and result2, average pageviews since de beginning of time
    d1 = datetime.strptime(START_ALL_TIME, "%Y%m%d")
-
    d2 = datetime.strptime(END_CURRENT_YEAR, "%Y%m%d")
-
    days = (abs((d2 - d1).days)+1)
-
    ARTICLE = article.replace(" ","_")
 
    SUM = 0
@@ -143,50 +120,32 @@ def visit(article):
    try:
 
       url ="https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/"+language+".wikipedia/all-access/user/"+ARTICLE+"/daily/"+START_ALL_TIME +"/" + END_CURRENT_YEAR
-
       html = urlopen(url).read()
-
       ecc = 0 # to change
-
       if ecc == 0:
-
          html = str(html)
-
          html = html.replace('{"items":[',"")
-
          html = html.replace(']}',"")
-
          n = html.count("}")
 
-
-
          for i in range(n):
-
             txt = html[:html.find("}")+1]
-
             SUM += int(txt[txt.find('"views":')+len('"views":'):-1])
-
             html =html.replace(txt,"",1)
 
-
-
          result1 = str(SUM)
-
          result2 = str(int(round((SUM/days),0)))
 
    except:
-
       result1 = "ERROR"
-
       result2 = "ERROR"
 
    #calculate result3, average pageviews from previous year
-   result3 = get_avg_pageviews(article, START_PREV_YEAR, END_PREV_YEAR)
+   result3 = get_avg_pageviews(article, START_PREV_YEAR, END_PREV_YEAR, language)
 
    #calculate result4, average pageviews from current year
-   result4 = get_avg_pageviews(article, START_CURRENT_YEAR, END_CURRENT_YEAR)
-
-      
+   result4 = get_avg_pageviews(article, START_CURRENT_YEAR, END_CURRENT_YEAR, language)
+   
    return str(result1), str(result2), str(result3), str(result4)
 
 
@@ -213,11 +172,8 @@ def dimension(text):
 
 # this function counts the occurrences of specific image file extensions within a given string and returns that count as a string.
 def images(text):
-
    t = text.lower()
-
    img = str(t.count('.jpg')+t.count('.svg')+t.count('.jpeg')+t.count('.png')+t.count('.tiff')+t.count('.gif')+t.count('.tif')+t.count('.xcf'))
-
    return img
 
 
@@ -227,7 +183,7 @@ def images(text):
 it reads the HTML content of the page and converts it's content to a string and then extracts a specific portion of the HTML using string manipulation. 
 Specifically, it looks for the substring "created_at" and extracts the following 10 characters, which should represent the creation date of the Wikipedia article.'''
 
-def first_edit(article):
+def first_edit(article, language):
    try:
 
       url ="https://xtools.wmflabs.org/api/page/articleinfo/"+language+".wikipedia.org/"+article.replace(" ","_")
@@ -249,60 +205,47 @@ def first_edit(article):
 
 
 # This function analyzes and count specific types of tags within a given text. The results are returned as strings, making them suitable for further processing.
-def warnings(t):
+
+def warnings(t): 
 
    t_tmp =t
    t = t.replace("\n","")
    t = t.replace(" ","")
    t = t.lower()
-
+   # tmp  == template
     
-   tmp_to_check = t.count('{{c|') + t.count('{{c}}')
+   with open("language_template_config.json", "r") as config_file:
+      language_template_config = json.load(config_file)
 
-   tmp_synoptic = t.count('{{tmp|')  + t.count('{{tmp}}')
+   if WIKIPEDIA_LANGUAGE in language_template_config:
+      language_config = language_template_config[WIKIPEDIA_LANGUAGE]
+   else:
+      print(f"Configuration not found for language '{WIKIPEDIA_LANGUAGE}'.") # Handle this case appropriately (e.g, exit the script).
 
-   tmp_help = t.count('{{a|')
-
-   tmp_correct = t.count('correggere{{')
-
-   tmp_curiosity = t.count('{{curiosit')
-
-   tmp_divide = t.count('{{d|') + t.count('{{d}')
-
-   tmp_sources = t.count('{{f|')  + t.count('{{f}}')
-
-   tmp_localism = t.count('{{l|')  + t.count('{{l}}')
-
-   tmp_pov = t.count('{{p|')  + t.count('{{p}}')
-
-   tmp_nn= t.count('{{nn|')  + t.count('{{nn}}')
-
-   tmp_recentism = t.count('{{recentismo')
-
-   tmp_manual_style = t.count('{{stilemanualistico')
-
-   tmp_translation = t.count('{{t|')  + t.count('{{t}}')
-
-   tmp_wikificare = t.count('{{w|')  + t.count('{{w}}')
-
-   tmp_stub = t.count('{{s|')  + t.count('{{s}}')
-
-   tmp_stub_section = t.count('{{stubsezione')
-
-   tmp_copy_control = t.count('{{controlcopy')
-
-
+   tmp_to_check = sum(t.count(template) for template in language_config.get("to_check"))
+   tmp_synoptic = sum(t.count(template) for template in language_config.get("synoptic"))
+   tmp_help = sum(t.count(template) for template in language_config.get("help"))
+   tmp_correct = sum(t.count(template) for template in language_config.get("correct"))
+   tmp_curiosity = sum(t.count(template) for template in language_config.get("curiosity"))
+   tmp_divide = sum(t.count(template) for template in language_config.get("divide")) 
+   tmp_sources = sum(t.count(template) for template in language_config.get("sources"))
+   tmp_localism = sum(t.count(template) for template in language_config.get("localism"))
+   tmp_pov = sum(t.count(template) for template in language_config.get("pov"))
+   tmp_nn = sum(t.count(template) for template in language_config.get("nn"))
+   tmp_recentism = sum(t.count(template) for template in language_config.get("recentism"))
+   tmp_manual_style = sum(t.count(template) for template in language_config.get("manual_style"))
+   tmp_translation = sum(t.count(template) for template in language_config.get("translation"))
+   tmp_wikificare = sum(t.count(template) for template in language_config.get("wikificare")) 
+   tmp_stub = sum(t.count(template) for template in language_config.get("stub"))
+   tmp_stub_section = sum(t.count(template) for template in language_config.get("stub_section"))
+   tmp_copy_control = sum(t.count(template) for template in language_config.get("copy_control"))
 
    sum_of_warnings = tmp_to_check + tmp_synoptic + tmp_help + tmp_correct + tmp_curiosity + tmp_divide + tmp_sources + tmp_localism + tmp_pov
    sum_of_warnings += tmp_nn + tmp_recentism + tmp_manual_style + tmp_translation + tmp_wikificare + tmp_stub + tmp_stub_section + tmp_copy_control
 
 
-
-   tmp_without_sources = t.count('{{senzafonte') + t.count('{{citazionenecessaria') + t.count('{{senzafonte}}') + t.count('{{citazionenecessaria}}')
-
-   tmp_to_clarify = t.count('{{chiarire') + t.count('{{chiarire}}')
-
-
+   tmp_without_sources = sum(t.count(template) for template in language_config.get("without_sources"))
+   tmp_to_clarify = sum(t.count(template) for template in language_config.get("clarify"))
 
    return str(sum_of_warnings), str(tmp_without_sources), str(tmp_to_clarify)
 
@@ -334,43 +277,28 @@ def find_template(text):
 # Function to calculate the length of the introduction
 #Incipit means the opening of a manuscript, early printed book. Hence, incipit == introduction 
 def calculate_introduction_length(text):
-
    incipit = text
-
    incipit = incipit[:incipit.find("\n==")]
-
    template_count  =incipit.count('{{')
 
    # incipitclear = incipit
-
    format_num = incipit.count("{{formatnum:")
 
    for i in range(format_num):
-
       tmp = incipit[incipit.find("{{formatnum:"):]
-
       tmp = tmp[:tmp.find("}}")+2]
-
       tmp2 = tmp.replace("{{formatnum:","")
 
       tmp2 = tmp2.replace("}}","")
-
       incipit = incipit.replace(tmp, tmp2)
-
-
 
    template_count = incipit.count("{{")
 
    for i in range(template_count):
-
       text = incipit[incipit.find("{{"):]
-
       template = find_template(text)
-
       text = text.replace("{{"+template,"")
-
       incipit = incipit.replace("{{"+template,"")
-
    incipit = incipit.replace("</ref>","")
 
    n = incipit.count("<ref")
@@ -378,17 +306,12 @@ def calculate_introduction_length(text):
    for i in range(n):
 
       tmp = incipit[incipit.find("<ref"):]
-
       tmp = tmp[:tmp.find(">")+1]
-
       incipit = incipit.replace(tmp,"")
 
    incipit = incipit.replace("[[","")
-
    incipit = incipit.replace("]]","")
-
    incipit = incipit.replace("|","")
-
    introduction_length = len(incipit)
    return str(introduction_length)
 
@@ -396,16 +319,16 @@ def calculate_introduction_length(text):
 
 
 # Function to check if the article is a "good article"
-def vdq(text):
-   if template_quality in text.lower():
+def vdq(text, display_window_template):
+   if display_window_template in text.lower():
       return "1"
 
    else:
       return "0"
 
 
-# This function checks to if a specific template (defined by the vdqTemplate variable) is present in the provided text.
-def featured_in(text):
+# This function checks to if a specific template (defined by the featured_template variable) is present in the provided text.
+def featured_in(text, featured_template):
    if featured_template in text.lower():
       return "1"
 
@@ -415,9 +338,11 @@ def featured_in(text):
 
     
 # Main analysis function
-def analysis():
-   # f = open('query.csv', "r") #Adding a character encoding will required for some characters in the query.csv file to avoid getting a UnicodeDecodeError
-   f = open('query.csv', 'r')
+def analysis(language, file_to_be_analysed, display_window_template, discussionURL,warnings_config, discussion_size, 
+             incipit_size, common_gallery,common_pages, itwikisource, coordinate, featured_template):
+   
+   # f = open('query.csv', "r") #Adding a character encoding will be required for some characters in the query.csv file to avoid getting a UnicodeDecodeError
+   f = open(file_to_be_analysed, 'r', encoding= "utf-8" )  #change this to file to read as an option from users
 
    articles = f.readlines()   
     
@@ -432,12 +357,11 @@ def analysis():
       flag = 1
 
       article = article[:-1]
-      article= article.replace(" ","_")
+      article= article.replace(" ","_") # Wikipedia page titles are case-sensitive and spaces in page titles should be replaced with underscores.
       result = ""
       wikitext = ""
 
-      article2 = urllib.parse.quote(article)
-      article = article.replace(" ","_")
+      article2 = urllib.parse.quote(article) 
 
 
       try:
@@ -446,6 +370,14 @@ def analysis():
          json_url = urlopen(url)
 
          data = json.loads(json_url.read())
+
+         # for debuging
+         # if "error" in data:
+         #    error_message = data["error"]["info"]
+         #    print(f"Error for article '{article}': {error_message}")
+         #    result = result + article + "\t" + error_message  # Include the error message in the result
+         # else:
+         #    wikitext = data["parse"]["wikitext"]
 
          wikitext = data["parse"]["wikitext"]
 
@@ -461,23 +393,19 @@ def analysis():
          pass                                     
 
       try:
-
+         article = article.replace(" ","_")
          article2 = urllib.parse.quote(article)
 
-         article = article.replace(" ","_")
-
+         
          url = "https://"+language+".wikipedia.org/w/api.php?action=query&titles=" + article2 +"&prop=pageprops&format=json&formatversion=2"
 
          json_url = urlopen(url)
-
          data = json.loads(json_url.read())
-
          wikidataid = data["query"]["pages"][0]["pageprops"]["wikibase_item"]
 
          url ="https://www.wikidata.org/wiki/Special:EntityData/"+wikidataid+".json"
 
          json_url = urlopen(url)
-
          wikidata = json.loads(json_url.read())
 
 
@@ -509,14 +437,14 @@ def analysis():
          result = result + wikidataid + "\t"
 
       except:
-         result = result + article +"\t" +"voice non-existent"
+         result = result + article +"\t" +" article non-existent"
 
        
 
       else:
 
          if first_edit:
-            result = result + first_edit(article2) + "\t"
+            result = result + first_edit(article2, language) + "\t"
 
          if dimension:
             result = result + dimension(wikitext) + "\t"
@@ -542,16 +470,15 @@ def analysis():
 
 
          if visit:
-            for i in visit(article2):
+            for i in visit(article2, language):
                result = result + i + "\t"
 
          if vdq:
-            result = result + vdq(wikitext) + "\t"
+            result = result + vdq(wikitext, display_window_template) + "\t"
 
 
-
-         if featured:
-            result = result + featured_in(wikitext) + "\t"
+         if featured_in:
+            result = result + featured_in(wikitext, featured_template) + "\t"
 
 
 
@@ -592,9 +519,6 @@ def analysis():
       print (result)
 
      
-
-def main():
-   analysis()
 
 
 
---
+++ data-gathering/serialize_data.py
@@ -0,0 +1,172 @@
+
+
+
+
+import json
+import urllib.parse
+
+# Define a dictionary or configuration file for each Wikipedia language
+discussion = "Discusión:"
+wikipedia_config = {
+    "es": {
+        "file_to_be_analysed": "query.csv",
+         "language": "es",
+         "id_wikidata": 1,
+         "dimension": 1,
+         "first_edit": 1,
+         "note": 1,
+         "images": 1,
+         "views": 1,
+         "incipit_size": 1,
+         "discussion_size": 1,
+         "discussionURL": urllib.parse.quote("discusión:"),  # Discussion Page Prefix
+         "warnings_config": 0,
+         "common_pages": 1,
+         "common_gallery": 1,
+         "itwikisource": 1,
+         "wikiversity": 1,
+         "wikibooks": 1,
+         "featured_in": 1,
+         "quality": 1,
+         "review": 1,
+         "bibliography": 1,
+         "coordinate": 1,
+         "featured_template": "{{artículo destacado",  # display_window Template
+         "display_window_template": "{{artículo bueno",  # Quality Template
+      },
+    "it": {
+        "file_to_be_analysed": "query.csv", #change to italian csv later
+         "language": "it",
+         "id_wikidata": 1,
+         "dimension": 1,
+         "first_edit": 1,
+         "note": 1,
+         "images": 1,
+         "views": 1,
+         "incipit_size": 1,
+         "discussion_size": 1,
+         "discussionURL": urllib.parse.quote("discussione:"),  # Discussion Page Prefix
+         "warnings_config": 0,
+         "common_pages": 1,
+         "common_gallery": 1,
+         "itwikisource": 1,
+         "wikiversity": 1,
+         "wikibooks": 1,
+         "featured_in": 1,
+         "quality": 1,
+         "review": 1,
+         "bibliography": 1,
+         "coordinate": 1,
+         "featured_template": "{{voce in vetrina",  # display_window Template (Italian)
+         "display_window_template": "{{voce buona",  # Quality Template (Italian)
+      },
+    "en": {
+        "file_to_be_analysed": "default.csv",
+         "language": "en",
+         "id_wikidata": 1,
+         "dimension": 1,
+         "first_edit": 1,
+         "note": 1,
+         "images": 1,
+         "views": 1,
+         "incipit_size": 1,
+         "discussion_size": 1,
+         "discussionURL": urllib.parse.quote("discussion:"),  # Discussion Page Prefix
+         "warnings_config": 0,
+         "common_pages": 1,
+         "common_gallery": 1,
+         "itwikisource": 1,
+         "wikiversity": 1,
+         "wikibooks": 1,
+         "featured_in": 1,
+         "quality": 1,
+         "review": 1,
+         "bibliography": 1,
+         "coordinate": 1,
+         "featured_template": "{{featured article",  # display_window Template (English)
+         "display_window_template": "{{good article",  # Quality Template (English)
+      },
+      # Add configurations for other Wikipedia languages as needed
+}
+
+language_template_config = {
+ "en": {
+    "to_check": ["{{cleanup}}"],
+     "synoptic": ["{{tmp|", "{{tmp}}"],
+    "help": ["{{a|"],
+    "correct": ["{{correct}}"],
+    "curiosity": ["{{curiosity}}"],
+    "divide": ["{{divide|", "{{divide}}"],
+    "sources": ["{{sources|", "{{sources}}"],
+    "localism": ["{{localism|", "{{localism}}"],
+    "pov": ["{{pov|", "{{pov}}"],
+    "nn": ["{{nn|", "{{nn}}"],
+    "recentism": ["{{recentism}}"],
+    "manual_style": ["{{manual style}}"],
+    "translation": ["{{translation}}"],
+    "wikificare": ["{{wikificare|", "{{wikificarw}}"],
+    "stub": ["{{stub|", "{{stub}}"],
+    "stub_section": ["{{stub section}}"],
+    "copy_control": ["{{control copy}}"],
+    "without_sources": ["{{unreferenced}}", "{{citation needed}}"],
+    "clarify": ["{{clarify}}"]
+  },
+  "es": {
+    "to_check": ["{{c|", "{{c}}"],
+    "synoptic": ["{{tmp|", "{{tmp}}"],
+    "help": ["{{a|"],
+    "correct": ["correggere"],
+    "curiosity": ["{{curiosit"],
+    "divide": ["{{d|", "{{d}}"],
+    "sources": ["{{f|", "{{f}}"],
+    "localism": ["{{l|", "{{l}}"],
+    "pov": ["{{p|", "{{p}}"],
+    "nn": ["{{nn|", "{{nn}}"],
+    "recentism": ["{{recienteismo"],
+    "manual_style": ["{{stilemanualistico"],
+    "translation": ["{{t|", "{{t}}"],
+    "wikificare": ["{{w|", "{{w}}"],
+    "stub": ["{{s|", "{{s}}"],
+    "stub_section": ["{{stubsezione"],
+    "copy_control": ["{{controlcopy"],
+    "without_sources": ["{{senzafonte", "{{citazionenecessaria","{{citazionenecessaria}}", "{{senzafonte}}"],
+    "clarify": ["{{chiarire", "{{chiarire}}"]
+  },
+  "it":{
+    "to_check": ["{{c|", "{{c}}"],
+    "synoptic": ["{{tmp|", "{{tmp}}"],
+    "help": ["{{a|"],
+    "correct": ["correggere"],
+    "curiosity": ["{{curiosit"],
+    "divide": ["{{d|", "{{d}"],
+    "sources": ["{{f|", "{{f}}"],
+    "localism": ["{{l|", "{{f}}"],
+    "pov": ["{{p|", "{{p}}}"],
+    "nn": ["{{nn|", "{{nn}}"],
+    "recentism": ["{{recentismo"],
+    "manual_style": ["{{stilemanualistico"],
+    "translation": ["{{stilemanualistico"],
+    "wikificare": ["{{w|", "{{w}}"],
+    "stub": ["{{s|", "{{s}}"],
+    "stub_section": ["{{stubsezione"],
+    "copy_control": ["{{controlcopy"],
+    "without_sources": ["{{senzafonte", "{{citazionenecessaria","{{citazionenecessaria}}"],
+    "clarify": ["{{chiarire", "{{chiarire}}"]
+  },
+    # Add more languages as required
+}
+
+
+
+# Serialize the data to JSON
+json_data = json.dumps(wikipedia_config, indent=4, ensure_ascii=False)
+language_json_data = json.dumps(language_template_config, indent=4, ensure_ascii=False)
+# Write the JSON data to a file
+with open("wikipedia_config.json", "w", encoding='utf-8') as json_file:
+    json_file.write(json_data)
+with open("language_template_config.json", "w", encoding='utf-8') as json_file:
+    json_file.write(language_json_data)
+
+
+# This code will create a JSON file named "wikipedia_config.json" with the provided data. 
+# You can add configurations for other Wikipedia languages as needed by extending the wikipedia_config dictionary
