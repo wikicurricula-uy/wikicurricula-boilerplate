f6e2d02e5f448094135597c5d61e8bb2c93616c4
Samuel Alison <samuelalison13@gmail.com>
2023-10-24 09:50:56 +0100

    added js functionalities to darkmode

--- visualization/assets/script/dark-mode.js
+++ visualization/assets/script/dark-mode.js
@@ -1,5 +1,5 @@
 function toggleDarkMode() {
-    
+    const toTheTop = document.querySelector('.to-top');
     const darkModeButton = document.getElementById('darkModeButton');
     const body = document.body;
     const svgElements = document.querySelectorAll('.svg-element'); 
@@ -11,6 +11,8 @@ function toggleDarkMode() {
      });
     
     if (body.classList.contains('dark-mode')) {
+        toTheTop.classList.add('to-top-dark');
+        toTheTop.classList.remove('to-top-light');
         darkModeButton.innerHTML = `
 
             <svg id="sunIcon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-brightness-high-fill" viewBox="0 0 16 16">
@@ -18,6 +20,8 @@ function toggleDarkMode() {
             </svg>
         `;
     } else {
+        toTheTop.classList.add('to-top-light');
+        toTheTop.classList.remove('to-top-dark');
         darkModeButton.innerHTML = `
         <svg id="moonStarsIcon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-moon-stars-fill" viewBox="0 0 16 16">
             <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/>
